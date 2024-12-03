ea31f42d1980007d9422322c63272ca393d4f7e7
tang <1487046534@qq.com>
2024-12-03 11:01:26 +0800

    feat: 项目初始化

---
+++ src/main/java/org/novel/App.java
@@ -0,0 +1,13 @@
+package org.novel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
